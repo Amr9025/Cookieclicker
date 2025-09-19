c35134192a2659f1e2ca7972480cbf9859424dab
Shadelio <shadeliocooman69@gmail.com>
2025-09-19 13:45:21 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,11 @@
+(function() {
+  let score = 0;
+  const scoreEl = document.getElementById('score');
+  const cookieBtn = document.getElementById('cookieBtn');
+
+  cookieBtn.addEventListener('click', () => {
+    score++;
+    scoreEl.textContent = score;
+  });
+})();
 
