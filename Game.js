167309a7c674da9b3ef92fb1c023a965456940af
Shadelio <shadeliocooman69@gmail.com>
2025-09-19 13:51:15 +0100

    Update Game.js

--- Game.js
+++ Game.js
@@ -1,1 +1,10 @@
+(function() {
+  let score = 0;
+  const scoreEl = document.getElementById('score');
+  const cookieBtn = document.getElementById('cookieBtn');
 
+  cookieBtn.addEventListener('click', () => {
+    score++;
+    scoreEl.textContent = score;
+  });
+})();
